e0bcda6cd6cd969acb5e3470af394f3ff885b6ff
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-10-13 16:24:28 +0100

    added npm icon flags and created icon images on cards

--- components/MCard.vue
+++ components/MCard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { meeows } from './meeows';
 import { DateTime } from "luxon";
-
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 const props = defineProps<{
     meeow: {
@@ -28,18 +28,27 @@ const suffix = computed(() => {
 
 <template>
     <div>
-        <div class="rounded-tl-xl rounded-br-xl m-10 bg-meeow-green w-96 h-52 relative overflow-hidden">
-            <div class="rounded-full bg-white w-12 h-12 absolute top-4 right-4 flex items-center justify-center text-4xl">
+        <div class="flex rounded-tl-xl rounded-br-xl m-10 w-96 h-52 relative overflow-hidden">
+            <div class="rounded-full bg-white w-10 h-10 absolute top-4 right-6 flex items-center justify-center text-4xl">
                 1</div>
-            <img :src="meeow.imageUrl" class="bg-cover" />
-            <m-button class="rounded-sm px-6 py-2 bg-white absolute bottom-2 right-2" color="white" :class="[`text-$`]">Book
+            <div class="rounded-br-xl">
+                <img :src="meeow.imageUrl" class="bg-cover" />
+            </div>
+            <m-button class="rounded-md px-4 py-1 bg-white absolute bottom-2 right-6 text-center justify-center items-center text-opacity-0" color="white">Book
                 ahead</m-button>
+            <div class="rounded-full bg-white w-10 h-10 absolute top-20 right-6 flex items-center justify-center overflow-hidden border border-white">
+                <div class="bg-cover bg-center !h-10 !w-10 relative bottom-1">
+                    <span class="fi fi-gb fis !h-10 !w-10 fis"></span>
+                </div>
+            </div>
+
         </div>
-        <div class="flex flex-col mx-10">
+        <div class="flex flex-col mx-10 space-y-1">
             <span class="text-2xl font-bold max-w-md"> {{ meeow.title }}</span>
-            <span class="text-lg"> {{ meeow.location }} {{ meeow.country }}</span>
-            <span class="text-lg"> {{ date.day }}{{ suffix }} {{ date.monthLong }} {{ date.year }} - {{ date.hour }}  <span class="lowercase">{{ date.toFormat('a') }}</span></span>
-                        <!-- <span class="text-lg"> {{ `${date.day} ${date.monthLong} ${date.year} - ${date.hour} ` }}<span class="lowercase">{{ date.toFormat('a') }}</span></span> -->
+            <span class="text-lg"> {{ meeow.location }}</span>
+            <span class="text-lg"> {{ date.day }}{{ suffix }} {{ date.monthLong }} {{ date.year }} - {{ date.hour }} <span
+                    class="lowercase">{{ date.toFormat('a') }}</span></span>
+            <!-- <span class="text-lg"> {{ `${date.day} ${date.monthLong} ${date.year} - ${date.hour} ` }}<span class="lowercase">{{ date.toFormat('a') }}</span></span> -->
             <p>Unlimited spaces</p>
             <span class="text-lg max-w-md underline"> {{ meeow.category.name }}</span>
 
