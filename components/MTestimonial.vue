04a2849789973202b01f34b04a3195587329c524
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-10-13 16:53:25 +0100

    updated testimonials

--- components/MTestimonial.vue
+++ components/MTestimonial.vue
@@ -1,5 +1,39 @@
 <script setup lang='ts'>
 
+const testimonials = [
+    {
+        message: 'So many of the connections I’ve made on Meeow have turned into really great business relationships…',
+        name: 'Anna Iveson',
+        company: 'Wordistry',
+        country: 'United Kingdom',
+        image: '/anna-iveson.png',
+    },
+    {
+        message: 'Meeow is the best way to continue conversations and collaborations after online events…',
+        name: 'Stuart Clarke',
+        company: 'Leeds Digital Festival',
+        country: 'United Kingdom',
+        image: '/stuart-clarke.png',
+
+    },
+    {
+        message: "Meeow isn't just the best way of networking online, it's the best way of networking full stop.",
+        name: 'Tara Boyd',
+        company: 'Lived Health',
+        country: 'United Kingdom',
+        image: '/tara-boyd.png'
+
+    },
+    {
+        message: "Meeow is a great way to have real conversations online, and the format of 4 people keeps things lively.",
+        name: 'Mitali Mookerjee',
+        country: 'United Kingdom',
+        image: '/mitali-mookerjee.png',
+    }
+]
+
+// count, click next, next item in the array
+const count = ref(1);
 </script>
 <template>
     <div class='relative flex flex-col w-[35%] m-20 pr-64 pb-40 mb-40'>
@@ -8,13 +42,11 @@
         <div class="white box pt-20 w-[110%]">
             <div class="rounded-sm ml-96 w-[60%] py-10 pl-10 text-black bg-white">
                 <p class="w-[400px] text-2xl whitespace-wrap">
-                    So many of the
-                    connections I’ve made on Meeow have
-                    turned into really great business relationships…</p>
+                    {{ testimonials[count].message }}</p>
                 <!-- Person details -->
                 <div class="flex flex-row text-sm bold whitespace-pre-line py-5">
                     <h5>
-                        Anna Iveson, Wordistry
+                        {{ testimonials[count].name }}
                     </h5>
                     <h5 class="pl-5">
                         United Kingdom
@@ -32,6 +64,6 @@
             </div>
         </div>
 
-        <img class="absolute bottom-14 right-20 w-[207.74px]" src="@/public/Woman.png" />
+        <img class="absolute bottom-14 right-20 w-[207.74px]" :src="testimonials[count].image" />
     </div>
 </template>
